1ebc3465948c58a75020a274cec04e158873a10a
Mathias5g <capmathiasdatp@gmail.com>
2021-12-01 23:55:24 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import {SimioController} from "./controller/SimioController";
 
 const app = express();
 app.use(express.json());
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'content-type');
+	res.setHeader('Content-Type', 'application/json');
+	// @ts-ignore
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 
 app.post("/simian", new SimioController().isSimian)
 app.get("/stats", new SimioController().checarStats)
